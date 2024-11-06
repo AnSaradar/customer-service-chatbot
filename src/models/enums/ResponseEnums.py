1d399ee76cfd865e347eaecc577c059a54686347
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-11-06 14:30:27 +0300

    Adding the upload endpoint

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -4,3 +4,12 @@ class ResponseSignal(Enum):
 
     SERVER_IS_DOWN = "Server is down"
     SERVER_IS_UP = "Server is up"
+
+    ERROR_FILE_FORMAT_NOT_ALLOWED = "File format not supported"
+    ERROR_FILE_MAX_SIZE_EXCEEDED = "File  max size exceeded"
+
+    FILE_VALIDATION_SUCCESS = "File validation succeeded"
+    FILE_VALIDATION_FAILED = "File validation failed"
+
+    FILE_UPLOADED_SUCCESS = "File uploaded successfully"
+    FILE_UPLOADED_FAILED = "File upload failed"

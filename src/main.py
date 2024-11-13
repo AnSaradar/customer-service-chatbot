d39cf9c299b4bc97db7791bc14e34263b507de5f
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-11-13 11:18:32 +0300

    setup the mongoDB connection

--- src/main.py
+++ src/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
 from routes import base_router, data_router, admin_router
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import Settings, get_settings
+import logging
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('uvivorn.error')
+
+@app.on_event("startup")
+async def startup():
+    settings = get_settings()
+    try:
+        app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+        app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+        logger.info(f"From Main Script: Connected to MongoDB at {settings.MONGODB_URL}")
+    except Exception as e:
+        logger.error(f"From Main Script: Error connecting to MongoDB: {str(e)}")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.mongo_conn.close()
+
+
 
 app.include_router(base_router)
 app.include_router(data_router)

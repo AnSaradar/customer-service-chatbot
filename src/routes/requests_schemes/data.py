678df92fdecc3d43a7e0a04e8e390a5e15832850
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-11-14 11:53:59 +0300

    Modifying the Process Request Scheme

--- src/routes/requests_schemes/data.py
+++ src/routes/requests_schemes/data.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ProcessRequest(BaseModel):
+    file_id : str # file name
     chunk_size : Optional[int] = 500
     overlap_size : Optional[int] = 20
-    
+    do_reset : Optional[int] = 0

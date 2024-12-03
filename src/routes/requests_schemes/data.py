5608bec1a6de797feb6498a16dc4316d68f9d134
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-12-03 13:54:53 +0300

    Update the Process file Request schema

--- src/routes/requests_schemes/data.py
+++ src/routes/requests_schemes/data.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ProcessRequest(BaseModel):
+    project_id: str
     file_id : str # file name
     chunk_size : Optional[int] = 500
     overlap_size : Optional[int] = 20

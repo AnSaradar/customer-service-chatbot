bfd286eb281a93b001c0ee32280ad07a209bc319
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-12-04 10:41:10 +0300

    Update the NLP Router Schemas

--- src/routes/requests_schemes/nlp.py
+++ src/routes/requests_schemes/nlp.py
@@ -6,6 +6,12 @@ class IndexProjectRequest(BaseModel):
     do_reset : Optional[int] = 1
 
 
+class InfoProjectRequest(BaseModel):
+    project_id : str
+
+
 class IndexSearchRequest(BaseModel):
+    # TODO: Add Chat history, and creativity factor (Temprature)
+    project_id : str
     text : str
     limit : Optional[int] = 5

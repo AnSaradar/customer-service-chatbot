8085523da20e4adf41f7afd6ac13da0c1b1b1595
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-12-16 16:33:29 +0300

    Update Config Schema:
    - Add validator for both email & phone

--- src/models/db_schemes/config_schema.py
+++ src/models/db_schemes/config_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field, validator
 from typing import Optional
 from bson.objectid import ObjectId
+import re
 
 class ConfigSchema(BaseModel):
     tempreature : float 
@@ -21,4 +22,19 @@ class ConfigSchema(BaseModel):
     def validate_file_max_size(cls, value):
         if value <= 0.0:
             raise ValueError("File max size must be greater than 0.")
+        return value
+    
+    @validator("contact_phone")
+    def validate_phone_number(cls, value):
+        # Regular expression for E.164 phone number format
+        e164_regex = re.compile(r"^\+?[1-9]\d{1,14}$")
+        if not e164_regex.match(value):
+            raise ValueError("Invalid phone number format. Must be in E.164 format (e.g., +1234567890).")
+        return value
+    
+    @validator('contact_email')
+    def validate_email(cls, value):
+        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(email_regex, value):
+            raise ValueError('Invalid email format')
         return value

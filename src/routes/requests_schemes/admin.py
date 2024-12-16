9fd6fae93facc96aaedb73bb4de582a09ea2184b
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2024-12-16 15:21:36 +0300

    Update admin requests schemas

--- src/routes/requests_schemes/admin.py
+++ src/routes/requests_schemes/admin.py
@@ -13,8 +13,9 @@ class CreateProjectRequest(BaseModel):
     #     return value
 
 class ConfigUpdateRequest(BaseModel):
-    APP_NAME: Optional[str] = get_settings().APP_NAME
-    APP_VERSION: Optional[str] = get_settings().APP_VERSION
-    FILE_ALLOWED_TYPES: Optional[list[str]] = get_settings().FILE_ALLOWED_TYPES
-    FILE_MAX_SIZE: Optional[int] = get_settings().FILE_MAX_SIZE
-    FILE_DEFAULT_CHUNK_SIZE: Optional[int] = get_settings().FILE_DEFAULT_CHUNK_SIZE
+    tempreature : float 
+    contact_email : str
+    contact_phone : str
+    file_max_size : float 
+    
+    
